0c9ec1b821cdd4b744203f0ada93452be221b33f
Luisa Méndez <mendezgarciafer89@gmail.com>
2026-03-25 18:29:39 -0500

    Add improved home page design

---
+++ app.py
@@ -0,0 +1,75 @@
+from flask import Flask, render_template, request
+import pickle
+import numpy as np
+
+app = Flask(__name__)
+
+try:
+    model = pickle.load(open('model.pkl', 'rb'))
+except:
+    model = None
+
+
+# HOME
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+# USE CASES
+@app.route('/use-cases')
+def use_cases():
+    return render_template('use_cases.html')
+
+#caso 1:
+@app.route('/use-case1')
+def use_case1():
+    return render_template('use_case1.html')
+#caso 2
+@app.route('/use-case2')
+def use_case2():
+    return render_template('use_case2.html')
+
+#caso 3
+@app.route('/use-case3')
+def use_case3():
+    return render_template('use_case3.html')
+
+#caso 4
+@app.route('/use-case4')
+def use_case4():
+    return render_template('use_case4.html')
+
+
+# LINEAR REGRESSION PAGE
+
+@app.route('/regression')
+def regression():
+    return render_template('regression.html')
+
+
+# PREDICTION
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if model is None:
+        return "Model not trained yet"
+
+    try:
+        year = int(request.form['year'])
+        mileage = int(request.form['mileage'])
+
+        prediction = model.predict([[year, mileage]])
+        price = round(prediction[0], 2)
+
+        return render_template('result.html', price=price)
+
+    except:
+        return "Error in input data"
+
+
+# RUN APP
+
+if __name__ == "__main__":
+    app.run(debug=True)
